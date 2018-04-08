02549955e6ff2d247dbdfffb28047f179a5a3817
Alan Yin <alanyin97@gmail.com>
2018-04-08 15:36:52 -0400

    Add connection to result screen

---
+++ DoggyDetective/app/src/main/java/com/gmanuel/doggydetective/BreedDescription.java
@@ -0,0 +1,15 @@
+package com.gmanuel.doggydetective;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class BreedDescription extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_breed_description);
+    }
+}
--- DoggyDetective/app/src/main/java/com/gmanuel/doggydetective/CharacteristicsInput.java
+++ DoggyDetective/app/src/main/java/com/gmanuel/doggydetective/CharacteristicsInput.java
@@ -1,13 +1,42 @@
 package com.gmanuel.doggydetective;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CharacteristicsInput extends AppCompatActivity {
-
+    private Button next;
+    private Button back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_characteristics_input);
+        next = findViewById(R.id.next);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openBreedDescription();
+            }
+        });
+        back = findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openPictureInput();
+            }
+        });
     }
+
+    public void openBreedDescription(){
+        Intent next = new Intent(this, BreedDescription.class);
+        startActivity(next);
+    }
+
+    public void openPictureInput(){
+        Intent back = new Intent(this, PictureInput.class);
+        startActivity(back);
+    }
+
 }
