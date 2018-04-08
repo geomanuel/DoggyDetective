6ef729a253cfd7c55ebe6480f79fa684b6185ddc
Alan Yin <alanyin97@gmail.com>
2018-04-08 15:48:08 -0400

    Button action added for result screen

--- DoggyDetective/app/src/main/java/com/gmanuel/doggydetective/BreedDescription.java
+++ DoggyDetective/app/src/main/java/com/gmanuel/doggydetective/BreedDescription.java
@@ -7,9 +7,35 @@ import android.view.View;
 import android.widget.Button;
 
 public class BreedDescription extends AppCompatActivity {
+    private Button yes;
+    private Button no;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_breed_description);
+        yes = findViewById(R.id.yes);
+        yes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openHome();
+            }
+        });
+        no = findViewById(R.id.no);
+        no.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openPictureInput();
+            }
+        });
+    }
+
+    public void openHome(){
+        Intent yes = new Intent(this, Home.class);
+        startActivity(yes);
+    }
+
+    public void openPictureInput(){
+        Intent no = new Intent(this, PictureInput.class);
+        startActivity(no);
     }
 }
