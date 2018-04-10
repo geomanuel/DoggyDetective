71b813016721eb45de036e678d4b0cf1e014a288
Alan Yin <alanyin97@gmail.com>
2018-04-09 23:09:13 -0400

    1

--- DoggyDetective/app/src/main/java/com/gmanuel/doggydetective/CharacteristicsInput.java
+++ DoggyDetective/app/src/main/java/com/gmanuel/doggydetective/CharacteristicsInput.java
@@ -3,21 +3,13 @@ package com.gmanuel.doggydetective;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Spinner;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-
 
 public class CharacteristicsInput extends AppCompatActivity {
     private Button next;
     private Button back;
-    public String behaviourResult;
-    private Spinner behaviour;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
